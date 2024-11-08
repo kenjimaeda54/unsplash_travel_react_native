a96a6e856ef4ee88349d96a5d374be165a6e5eda
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2024-11-08 07:26:28 -0300

    feat: finished configuration bottom tab

--- src/route/CustomBottomTab.tsx
+++ src/route/CustomBottomTab.tsx
@@ -2,30 +2,38 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../ui/screens/home/HomeScreen";
 import FavoriteScreen from "../ui/screens/favorite/FavoriteScreen";
 import ProfileScreen from "../ui/screens/profile/ProfileScreen";
-import HomeIcon from "../../assets/icons/home.svg"
-import HeartIcon from "../../assets/icons/heart.svg"
-import ProfileIcon from "../../assets/icons/profile.svg"
+import HomeIconNotFill from "../../assets/icons/home_not_fill.svg"
+import HomeIconFill from "../../assets/icons/home_fill.svg"
+import HeartIconNotFill from "../../assets/icons/heart_not_fill.svg"
+import HeartIconFill from "../../assets/icons/heart_fill.svg"
+import ProfileIconFill from "../../assets/icons/profile_fill.svg"
+import ProfileIconNoFill from "../../assets/icons/profile_not_fill.svg"
+
 
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function CustomBottomTab() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator screenOptions={{
+      headerShown: false, tabBarShowLabel: false,
+      tabBarActiveTintColor: "#000000",
+      tabBarInactiveTintColor: "#808080"
+    }}>
       <Screen options={{
-        tabBarIcon: ({ color }) => <HomeIcon color={color} width={20} height={20} />
+        tabBarIcon: ({ color, focused }) => focused ? <HomeIconFill fill={color} width={20} height={20} /> : <HomeIconNotFill fill={color} width={20} height={20} />
       }}
         name="home"
         component={HomeScreen} />
       <Screen
         options={{
-          tabBarIcon: ({ color }) => <HeartIcon color={color} width={20} height={20} />
+          tabBarIcon: ({ color, focused }) => focused ? <HeartIconFill fill={color} width={20} height={20} /> : <HeartIconNotFill fill={color} width={20} height={20} />
         }}
         name="favorite"
         component={FavoriteScreen} />
       <Screen
         options={{
-          tabBarIcon: ({ color }) => <ProfileIcon color={color} width={20} height={20} />
+          tabBarIcon: ({ color, focused }) => focused ? <ProfileIconFill fill={color} width={25} height={25} /> : <ProfileIconNoFill width={25} height={25} />
         }}
         name="profile" component={ProfileScreen} />
     </Navigator>
